fedb180e2e079ebac7aa878c8d6e7c3bf640d86f
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2020-02-11 20:03:10 -0500

    simple render working

--- quicksilver-utils-ecs/examples/adventurer.rs
+++ quicksilver-utils-ecs/examples/adventurer.rs
@@ -4,13 +4,16 @@
 // Sprites - 16 rows, 13 columns
 // Each sprite - 32x32 pixels
 
-use log::info;
+use log::{debug, info};
 use platter::load_file;
 use quicksilver::{
     graphics::{Graphics, Image, PixelFormat},
     lifecycle::{run, EventStream, Settings, Window},
     Result,
 };
+use quicksilver_utils_ecs::*;
+use send_wrapper::SendWrapper;
+use specs::prelude::*;
 use std::collections::HashMap;
 
 enum Orientation {
@@ -49,13 +52,53 @@ fn main() {
 }
 
 async fn app(window: Window, gfx: Graphics, mut event_stream: EventStream) -> Result<()> {
-    let sprite_sheet_data = load_file("test_assets/sprite_sheet.png").await?;
+    let sprite_sheet_data = load_file("sprite_sheet.png").await?;
     // let sprite_image = Image::from_raw(&gfx, Some(&sprite_sheet), 416, 512, PixelFormat::RGBA)?;
-    let sprite_image = Image::from_encoded_bytes(&gfx, &sprite_sheet_data);
+    let sprite_image: Image = Image::from_encoded_bytes(&gfx, &sprite_sheet_data)?;
+
+    debug!("Got the image");
+
+    let mut world = World::new();
+
+    world.insert(SharedRenderingContext {
+        gfx: SendWrapper::new(gfx),
+        window: SendWrapper::new(window),
+    });
+
+    world.register::<Position>();
+    world.register::<Sprite>();
+    world.register::<PlayerInputFlag>();
+
+    // Create the player
+    world
+        .create_entity()
+        .with(Position { x: 0., y: 0. })
+        .with(Sprite {
+            image: SendWrapper::new(sprite_image),
+        })
+        .with(PlayerInputFlag)
+        .build();
+
+    debug!("Created world, components, and entities");
+
+    // let mut dispatcher = DispatcherBuilder::new()
+    //     .with(RenderSprites, "render_sprites", &[])
+    //     .build();
+
+    let mut sprite_system = RenderSprites;
+
+    debug!("Entering main loop");
 
     'main: loop {
         info!("In the loop");
-        break 'main;
+
+        while let Some(ev) = event_stream.next_event().await {
+            debug!("Quicksilver event: {:?}", ev)
+        }
+
+        sprite_system.run_now(&world);
+
+        // dispatcher.dispatch_seq(&world) // otherwise SendWrapper will explode!
     }
 
     Ok(())
--- quicksilver-utils-ecs/src/lib.rs
+++ quicksilver-utils-ecs/src/lib.rs
@@ -1,29 +1,36 @@
+extern crate log;
 extern crate quicksilver;
 extern crate send_wrapper;
 extern crate specs;
 #[macro_use]
 extern crate specs_derive;
 
-use quicksilver::{graphics::Graphics, lifecycle::Event};
+use log::debug;
+use quicksilver::{
+    geom::Rectangle,
+    graphics::{Color, Graphics, Image},
+    lifecycle::{Event, Window},
+};
 use send_wrapper::SendWrapper;
 use specs::{prelude::*, Component, System, Write};
 
 #[derive(Component)]
-struct Position {
-    x: f32,
-    y: f32,
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
 }
 
 #[derive(Component)]
-struct Sprite {
-    dummy: bool,
+pub struct Sprite {
+    pub image: SendWrapper<Image>, // quicksilver graphics uses Rc
 }
 
 #[derive(Component)]
-struct PlayerInputFlag;
+pub struct PlayerInputFlag;
 
-struct SharedRenderingContext {
-    gfx: SendWrapper<Graphics>,
+pub struct SharedRenderingContext {
+    pub gfx: SendWrapper<Graphics>,  // quicksilver graphics uses Rc
+    pub window: SendWrapper<Window>, // quicksilver graphics uses *mut(0)
 }
 
 impl Default for SharedRenderingContext {
@@ -32,7 +39,7 @@ impl Default for SharedRenderingContext {
     }
 }
 
-struct RenderSprites;
+pub struct RenderSprites;
 
 impl<'a> System<'a> for RenderSprites {
     type SystemData = (
@@ -41,22 +48,34 @@ impl<'a> System<'a> for RenderSprites {
         Write<'a, SharedRenderingContext>,
     );
     fn run(&mut self, (position_storage, sprite_storage, mut ctx_resource): Self::SystemData) {
-        let gfx: &mut Graphics = &mut ctx_resource.gfx;
-
+        let ctx: &mut SharedRenderingContext = &mut ctx_resource;
+        ctx.gfx.clear(Color::WHITE);
+        debug!("Running RenderSprites");
         for (position, sprite) in (&position_storage, &sprite_storage).join() {
-            //
+            let sprite_position = Rectangle::new((0, 0), (32, 32));
+            let location_size = 64.;
+            let location = Rectangle::new(
+                (position.x, position.y),
+                (position.x + location_size, position.y + location_size),
+            );
+            debug!(
+                "Drawing dude from sprite {:?} at {:?}",
+                sprite_position, location
+            );
+            ctx.gfx
+                .draw_subimage(&sprite.image, sprite_position, location);
         }
-        // now loop over some sort of render component...
+        ctx.gfx.present(&ctx.window).expect("present"); // FIXME probably a separate system?
     }
 }
 
 // Could replace this with specific events
 #[derive(Default)]
-struct EventBuffer {
+pub struct EventBuffer {
     events: Vec<Event>,
 }
 
-struct WasdMovement;
+pub struct WasdMovement;
 
 impl<'a> System<'a> for WasdMovement {
     type SystemData = (
@@ -70,7 +89,7 @@ impl<'a> System<'a> for WasdMovement {
         (events_resource, player_input_flag_storage, mut position_storage): Self::SystemData,
     ) {
         let events: &EventBuffer = &events_resource;
-
+        debug!("Running WasdMovement");
         for (_flag, position) in (&player_input_flag_storage, &mut position_storage).join() {
             // Should only be one...
         }
